647401b05af47dc86571805135b7a661491d68c7
legajo8716 <nelgonzalez88@gmail.com>
2021-09-18 10:00:15 -0300

    agrego entrega 2

---
+++ entrega-2-Gonzalez-Nelson/entities/player/Player.gd
@@ -0,0 +1,10 @@
+extends Sprite
+onready var cannon :Sprite=$Cannon #llama a cannon una sola vez
+var speed = 200 #Pixeles
+
+func _physics_process(delta):
+	var direction_optimized:int = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
+	var mouse_position:Vector2=get_global_mouse_position()
+
+	cannon.look_at(mouse_position)
+	position.x += direction_optimized * speed * delta
