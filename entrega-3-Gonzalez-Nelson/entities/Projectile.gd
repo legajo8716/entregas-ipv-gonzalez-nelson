1dbba22ace7344e1ba525c28de5534bdc2b144b9
legajo8716 <nelgonzalez88@gmail.com>
2021-09-28 10:01:38 -0300

    juego sin bonus

--- entrega-3-Gonzalez-Nelson/entities/Projectile.gd
+++ entrega-3-Gonzalez-Nelson/entities/Projectile.gd
@@ -32,6 +32,3 @@ func _remove():
 	queue_free()
 	
 
-
-func _on_DetectionArea_body_entered(body):
-	if 
--- entrega-3-Gonzalez-Nelson/entities/enemies/Turret.gd
+++ entrega-3-Gonzalez-Nelson/entities/enemies/Turret.gd
@@ -20,12 +20,20 @@ func initialize(container, turret_pos,  projectile_container):
 func fire_at_player():
 	var proj_instance = projectile_scene.instance()
 	proj_instance.initialize(projectile_container, fire_position.global_position, fire_position.global_position.direction_to(target.global_position))
-func _on_DetectionArea_body_entered(body):
-	if target==null:
-		target=body
 
+
+
+func _on_Area2D_body_entered(body):
+	if body is Player:
+		print(body)
+		target=body
+		fire_timer.connect("timeout",self,"fire_at_player")
 		fire_timer.start()
-func _on_DetectionArea_body_exited(body):
-	if body==target:
-		target=null
+
+
+
+func _on_Area2D_body_exited(body):
+	if body is Player:
+		print("no")
+		target=body
 		fire_timer.stop()
